eaca869f374db52e5bbb65546a2f28fdc56b94f4
Daniel da silva pedro <danielsp699@outlook.com>
2022-04-23 17:34:00 -0300

    Initial commit - Contains:
    Creating Django Project
    Creating Models
    Creating DjangoAdmin User and using DjangoAdmin
    Show values on Website

---
+++ contatos/models.py
@@ -0,0 +1,46 @@
+from django.db import models
+from django.utils import timezone
+
+"""
+CONTATOS
+id: INT (automático)
+nome: STR * (obrigatório)
+sobrenome: STR (opcional)
+telefone: STR * (obrigatório)
+email: STR (opcional)
+data_criacao: DATETIME (automático)
+descricao: texto
+categoria: CATEGORIA (outro model)
+
+ CATEGORIA
+ id: INT
+ nome: STR * (obrigatório)
+"""
+
+
+# Create your models here.
+class Categoria(models.Model):
+    nome = models.CharField(max_length=255)
+
+    # Para mudar a exibição do nome no DjangoAdmin
+    def __str__(self):
+        return self.nome
+
+
+class Contato(models.Model):
+    nome = models.CharField(max_length=255)
+    sobrenome = models.CharField(max_length=255, blank=True)  # blank=True diz que o cmapo não é obrigatório
+    telefone = models.CharField(max_length=255)
+    email = models.CharField(max_length=255, blank=True)
+    data_criacao = models.DateTimeField(default=timezone.now)
+    descricao = models.TextField(blank=True)
+    categoria = models.ForeignKey(Categoria, on_delete=models.DO_NOTHING)  # Criando chave estrangeira referenciando
+
+    # classe Categoria
+
+    def __str__(self):
+        return self.nome
+
+# Para criar no SQLite, precisa fazer as migrações, são 2 comandos:
+# python .\manage.py makemigrations
+# python manage.py migrate
---
+++ contatos/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from .models import Contato
+
+
+# Create your views here.
+def index(request):
+    contatos = Contato.objects.all()
+    return render(request, 'contatos/index.html', {
+        'contatos': contatos
+    })
