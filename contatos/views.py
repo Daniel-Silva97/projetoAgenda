5b3fe0b9ee484baffdedf9a47e2a14ae3e1eb59e
Daniel da silva pedro <danielsp699@outlook.com>
2022-04-23 17:54:54 -0300

    Including page Details

--- contatos/views.py
+++ contatos/views.py
@@ -8,3 +8,10 @@ def index(request):
     return render(request, 'contatos/index.html', {
         'contatos': contatos
     })
+
+
+def see_contact(request, contato_id):
+    contato = Contato.objects.get(id=contato_id)
+    return render(request, 'contatos/see_contact.html', {
+        'contato': contato
+    })
