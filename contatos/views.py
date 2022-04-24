b6e002c4f41ca7e3b1deb758eff616020082b367
Daniel da silva pedro <danielsp699@outlook.com>
2022-04-24 15:18:45 -0300

    Including Order By and Filter

--- contatos/views.py
+++ contatos/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render, get_object_or_404
-# from django.http import Http404
+from django.http import Http404
 from .models import Contato
 from django.core.paginator import Paginator
 
 
 # Create your views here.
 def index(request):
-    contatos = Contato.objects.all()
+    # Ordenando a tabela por ID em ordem decrescente e filtrando somente os contatos que estão com mostrar=True
+    contatos = Contato.objects.order_by('-id').filter(mostrar=True)
 
     paginator = Paginator(contatos, 5)
     page = request.GET.get('p')
@@ -20,6 +21,11 @@ def index(request):
 def see_contact(request, contato_id):
     # contato = Contato.objects.get(id=contato_id)
     contato = get_object_or_404(Contato, id=contato_id)  # Forma mais simples de retornar erro 404
+
+    # Não exibindo os detalhes se o campo mostrar estiver = False
+    if not contato.mostrar:
+        raise Http404()
+
     return render(request, 'contatos/see_contact.html', {
         'contato': contato
     })
